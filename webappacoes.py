3b9c5f8af5e1b953c4dacab569d8cfa08cd847ed
Roger Santos <roger.informe@gmail.com>
2022-01-29 17:36:44 -0300

    atualizacao

--- webappacoes.py
+++ webappacoes.py
@@ -1,15 +1,10 @@
 import streamlit as st
 from pandas_datareader import data as pdr
-import yfinance as yf
 import pandas as pd
 from datetime import date
 import datetime
 from plotly import graph_objs as go
 
-yf.pdr_override() # <== that's all it takes :-)
-
-
-#DATA_FIM = date.today().strftime('%Y-%m-%d')
 
 # download dataframe
 st.title('Pesquisa de ações')
@@ -34,28 +29,62 @@ def pegar_valores_online(sigla_acao,inicio,fim):
     return df
 
 DATA_INICIO = st.sidebar.date_input(
-     "Escolha a data inicial",
-     datetime.date(2020, 1, 1))
+     "Escolha a data inicial. (ANO/MES/DIA)",
+     date.today() - datetime.timedelta(days=14))
 
 DATA_FIM = st.sidebar.date_input(
-     "Escolha a data final",
-     datetime.date(2022, 1, 1))
+     "Escolha a data final. (ANO/MES/DIA)",
+     date.today())
+
+if DATA_FIM < DATA_INICIO:
+    st.error('A data final dever ser mais recente que a data inicial')
 
 df_valores = pegar_valores_online(acao_escolhida,DATA_INICIO,DATA_FIM)
 
 st.subheader('Tabela de valores - ' + nome_acao_escolhida)
 st.write(df_valores)
 
-st.subheader('Gráfico de preços')
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=df_valores.index,
+
+st.subheader('Gráfico de preços')
+
+
+st.sidebar.subheader('Opções do gráfico')
+fech_check = st.sidebar.checkbox('Preço de fechamento',True)
+if fech_check:
+    fig.add_trace(go.Scatter(x=df_valores.index,
                          y=df_valores['Close'],
                          name='Preço fechamento',
                          line_color='yellow'))
-fig.add_trace(go.Scatter(x=df_valores.index,
+abert_check = st.sidebar.checkbox('Preço de abertura',True)
+if abert_check:
+    fig.add_trace(go.Scatter(x=df_valores.index,
                          y=df_valores['Open'],
                          name='Preço abertura',
                          line_color='blue'))
+
+fechajus_check = st.sidebar.checkbox('Fechamento Ajustado')
+if fechajus_check:
+    fig.add_trace(go.Scatter(x=df_valores.index,
+                         y=df_valores['Adj Close'],
+                         name='Fechamento ajustado',
+                         line_color='orange'))
+
+alta_check = st.sidebar.checkbox('Preço mais alto do dia')
+if alta_check:
+    fig.add_trace(go.Scatter(x=df_valores.index,
+                         y=df_valores['High'],
+                         name='Preço mais alto',
+                         line_color='green'))
+
+baixa_check = st.sidebar.checkbox('Preço mais baixo do dia')
+if baixa_check:
+    fig.add_trace(go.Scatter(x=df_valores.index,
+                         y=df_valores['Low'],
+                         name='Preço mais baixo',
+                         line_color='red'))
+else:
+    st.write('Selecione uma das opções do gráfico para ativá-lo')
 
 st.plotly_chart(fig)
 
